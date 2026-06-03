751a3464dfcd127c4f855c2932c5163713fd183c
Foontro <yuvraj17mishra11@gmail.com>
2026-06-03 19:50:54 +0530

    start from scratch

--- backend/services/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './db.service';
-export * from './llm.service';
-export * from './context.service';
-export * from './orchestrator.service';
-export * from './embedding.service';
-export * from './vector.service';
-export * from './rag.service';
-export * from './discord.service';
-
